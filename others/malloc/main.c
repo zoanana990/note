93dd75db5d1c95b9b5d3a007dfea824363cf0dc0
zoanana990 <harry021633@gmail.com>
2023-11-20 22:17:51 +0800

    reduce the structure of malloc

--- others/malloc/main.c
+++ others/malloc/main.c
@@ -4,24 +4,15 @@
 
 typedef long Align;
 
-union header {
-	struct {
-		union header *ptr;
-		unsigned int size;
-	} s;
-	Align x;
-};
-
-struct mm_list {
-    struct mm_list *ptr;
+struct header {
+    struct header *ptr;
     unsigned int size;
-    unsigned int blocks_unit;
 };
 
 void *my_malloc(unsigned int nbytes);
 void my_free(void *ap);
 
-typedef union header Header;
+typedef struct header Header;
 
 static unsigned char heaps[MAX_HEAPS];
 static unsigned char *program_break = heaps;
@@ -31,80 +22,92 @@ static Header *freep = NULL; /* start of free list */
 
 static void *sbrk(unsigned int nbytes)
 {
-	if (program_break + nbytes >= heaps
-	    && program_break + nbytes < heaps + MAX_HEAPS) {
-		unsigned char *previous_pb = program_break;
-		program_break += nbytes;
-		return (void *) previous_pb;
-	}
-	return (void *) -1;
+    if (program_break + nbytes >= heaps
+        && program_break + nbytes < heaps + MAX_HEAPS) {
+        unsigned char *previous_pb = program_break;
+        program_break += nbytes;
+        return (void *) previous_pb;
+    }
+    return (void *) -1;
 }
 
 void *my_malloc(unsigned int nbytes)
 {
-	Header *p, *prevp;
-	unsigned int nunits;
-	void *cp;
+    Header *p, *prevp;
+    unsigned int nunits;
+    void *cp;
 
-	nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+    /* Calculate the lowest size, including sizeof(Header) + real_malloc_size + padding
+     * In this program, there are 16 bytes per unit.
+     * */
+    nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
 
-	if ((prevp = freep) == NULL) {
-		base.s.ptr = freep = prevp = &base;
-		base.s.size = 0;
-	}
+    /* initialize the pointers, freep, prevp, base.ptr */
+    if ((prevp = freep) == NULL) {
+        base.ptr = freep = prevp = &base;
+        base.size = 0;
+    }
 
-	for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
-		if (p->s.size >= nunits) {
-			if (p->s.size == nunits) {
-				prevp->s.ptr = p->s.ptr;
-			} else {
-				p->s.size -= nunits;
-				p += p->s.size;
-				p->s.size = nunits;
-			}
-			freep = prevp;
-			return (void *)(p + 1);
-		}
 
-		if (p == freep) {
-			cp = sbrk(nunits * sizeof(Header));
-			if (cp == (void *) -1) {
-				return NULL;
-			} else {
-				p = (Header *) cp;
-				p->s.size = nunits;
-				my_free((void *) (p + 1));
-				p = freep;
-			}
-		}
-	}
+    for (p = prevp->ptr; ; prevp = p, p = p->ptr) {
+        if (p->size >= nunits) {
+            if (p->size == nunits) {
+                prevp->ptr = p->ptr;
+            } else {
+                p->size -= nunits;
+                p += p->size;
+                p->size = nunits;
+            }
+            freep = prevp;
+            return (void *)(p + 1);
+        }
+
+        if (p == freep) {
+
+            /* Break the boundary of heap */
+            cp = sbrk(nunits * sizeof(Header));
+            if (cp == (void *) -1) {
+                return NULL;
+            } else {
+                /* if breaking successfully,
+                 * then we update the unit to the structure */
+                p = (Header *) cp;
+                p->size = nunits;
+                my_free((void *) (p + 1));
+                p = freep;
+            }
+        }
+    }
 }
 
 void my_free(void *ap)
 {
-	Header *bp, *p;
-	bp = (Header *) ap - 1;
+    Header *bp, *p;
+    bp = (Header *) ap - 1;
 
-	for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr) {
-		if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
-			break;
-	}
+    for (p = freep; !(bp > p && bp < p->ptr); p = p->ptr) {
+        if (p >= p->ptr && (bp > p || bp < p->ptr))
+            break;
+    }
 
-	if (bp + bp->s.size == p->s.ptr) {
-		bp->s.size += p->s.ptr->s.size;
-		bp->s.ptr = p->s.ptr->s.ptr;
-	} else {
-		bp->s.ptr = p->s.ptr;
-	}
+    /* Merge two blocks */
+    if (bp + bp->size == p->ptr) {
+        bp->size += p->ptr->size;
+        bp->ptr = p->ptr->ptr;
+    } else {
+        bp->ptr = p->ptr;
+    }
 
-	if (p + p->s.size == bp) {
-		p->s.size += bp->s.size;
-		p->s.ptr = bp->s.ptr;
-	} else {
-		p->s.ptr = bp;
-	}
+    /* Merge previous block */
+    if (p + p->size == bp) {
+        p->size += bp->size;
+        p->ptr = bp->ptr;
+    } else {
+        p->ptr = bp;
+    }
 
-	freep = p;
+    /* update the p->ptr in freep */
+    freep = p;
 }
 
 int main() {
@@ -116,20 +119,25 @@ int main() {
     int *segment3 = my_malloc(sizeof(int) * 10);
     printf("segment 3 = %p\n", segment3);
     my_free(segment2);
+    my_free(segment1);
+    my_free(segment3);
     int *segment4 = my_malloc(sizeof(int) * 20);
     printf("segment 4 = %p\n", segment4);
     int *segment5 = my_malloc(sizeof(int) * 10);
     printf("segment 5 = %p\n", segment5);
-    my_free(segment1);
-    my_free(segment3);
+    int *segment6 = my_malloc(sizeof(int) * 20);
+    printf("segment 6 = %p\n", segment6);
     my_free(segment4);
     my_free(segment5);
+    int *segment7 = my_malloc(sizeof(int) * 20);
+    printf("segment 7 = %p\n", segment7);
+    my_free(segment6);
+    my_free(segment7);
     printf("Malloc test complete\n");
     printf("-----------------------------------------------------------\n");
-    printf("base.s.ptr = %p, base.s.size = %d\n", base.s.ptr, base.s.size);
-    printf("freep->s.ptr = %p, freep->s.size = %d\n", freep->s.ptr, freep->s.size);
+    printf("base.ptr = %p, base.size = %d\n", base.ptr, base.size);
+    printf("freep->ptr = %p, freep->size = %d\n", freep->ptr, freep->size);
     printf("program_break = %p\n", program_break);
-    printf("sizeof(struct mm_list) = %lu\n", sizeof(struct mm_list));
-    printf("sizeof(union header) = %lu\n", sizeof(union header));
+    printf("sizeof(struct header) = %lu\n", sizeof(struct header));
     return 0;
 }
